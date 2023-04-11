50a6e178c2ca2902d777f29dd2af31a0f1f5e9e7
jhonjpg <jonathan050315jj@gmail.com>
2023-04-10 23:26:43 -0400

    page not found

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageNotFound = () => {
   return (
-    <div className="w-100 bg-danger">PageNotFound</div>
+    <div className="w-100 h-100 bg-danger text-center fs-1">Pagee Not Found</div>
   )
 }
 
